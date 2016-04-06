46a4414cd30a3585ddfd14c17ca98a150aeaaad7
Rodrigo Fernandes <rtfrodrigo@gmail.com>
2016-04-06 13:57:51 +0100

    Add Builder generic implementation from Findbugs

---
+++ src/main/scala/codacy/dockerApi/traits/Builder.scala
@@ -0,0 +1,65 @@
+package codacy.dockerApi.traits
+
+import java.io.File
+import java.nio.file.Path
+
+import scala.util.{Success, Failure, Try}
+
+import codacy.dockerApi.utils.CommandRunner
+
+sealed trait Builder {
+  val command: List[String]
+  val pathComponents: Seq[String]
+  def supported(path: Path): Boolean
+  def targetOfDirectory(path: File): Option[String]
+
+  private def buildWithCommand(command: List[String], path: Path): Try[Boolean] = {
+    CommandRunner.exec(command, dir = Option(path.toFile)) match {
+      case Left(failure) => Failure(failure)
+      case Right(output) if output.exitCode != 0 =>
+        Failure(new Exception("Can't compile project."))
+
+      case Right(output) => Success(true)
+    }
+  }
+
+  def build(path: Path): Try[Boolean] = {
+    buildWithCommand(command, path)
+  }
+}
+
+object MavenBuilder extends Builder {
+  val command = List("mvn", "compile")
+  val pathComponents = Seq("src", "main", "java")
+
+  def supported(path: Path): Boolean = {
+    path.toFile.list.contains("pom.xml")
+  }
+
+  def targetOfDirectory(path: File): Option[String] = {
+    Some(Seq(path.getAbsolutePath, "target", "classes").mkString(File.separator))
+  }
+
+}
+
+object SBTBuilder extends Builder {
+  val command = List("sbt", "compile")
+  val pathComponents = Seq("src", "main", "scala")
+
+  def supported(path: Path): Boolean = {
+    path.toFile.list.contains("build.sbt")
+  }
+
+  def targetOfDirectory(path: File): Option[String] = {
+    val target = new File(Seq(path.getAbsolutePath, "target").mkString(File.separator))
+    target.exists match {
+      case true =>
+        val potentialScalaDir = target.list.filter { case filepath => filepath.startsWith("scala-")}
+        // TODO: Cleaner way to do this?
+        val scalaDirectory = potentialScalaDir.headOption.fold("") { case target => target}
+        Some(Seq(target.getAbsolutePath, scalaDirectory, "classes").mkString(File.separator))
+      case false =>
+        Option.empty
+    }
+  }
+}
