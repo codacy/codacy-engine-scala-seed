628408467d74a0cad58a320de51a470adb336640
Rodrigo Fernandes <rodrigo.fernandes@tecnico.ulisboa.pt>
2016-05-04 16:47:49 +0100

    Reduce docker timeout to 10 minutes

--- src/main/scala/codacy/dockerApi/DockerEngine.scala
+++ src/main/scala/codacy/dockerApi/DockerEngine.scala
@@ -21,7 +21,7 @@ abstract class DockerEngine(Tool: Tool) {
 
   lazy val timeout = Option(System.getProperty("timeout")).flatMap { case rawDuration =>
     Try(Duration(rawDuration)).toOption.collect { case d: FiniteDuration => d }
-  }.getOrElse(30.minutes)
+  }.getOrElse(10.minutes)
 
   lazy val isDebug = Option(System.getProperty("debug")).flatMap { case rawDebug =>
     Try(rawDebug.toBoolean).toOption
