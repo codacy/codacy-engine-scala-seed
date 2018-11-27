d343664ed0c2a3cc69aad489ab7abf14d1fbc798
Daniel Reigada <daniel@codacy.com>
2018-11-27 15:40:12 +0000

    Always use default values if the parameter's value is not set

--- src/main/scala/com/codacy/tools/scala/seed/utils/ToolHelper.scala
+++ src/main/scala/com/codacy/tools/scala/seed/utils/ToolHelper.scala
@@ -7,9 +7,21 @@ object ToolHelper {
   implicit class ConfigurationOps(configuration: Option[List[Pattern.Definition]]) {
 
     def withDefaultParameters(implicit spec: Tool.Specification): Option[List[Pattern.Definition]] = {
-      configuration.map(
-        _.map(pattern => pattern.copy(parameters = pattern.parameters.orElse(defaultParameters(pattern.patternId))))
-      )
+      configuration.map(_.map { pattern =>
+        def toMap(setOpt: Option[Set[Parameter.Definition]]): Option[Map[Parameter.Name, Parameter.Definition]] =
+          setOpt.map(paramSet => paramSet.map(p => (p.name, p))(collection.breakOut))
+
+        val configurationParameters = toMap(pattern.parameters)
+        val defaults = toMap(defaultParameters(pattern.patternId))
+
+        val parameters =
+          configurationParameters
+            .map(defaults.getOrElse(Map.empty) ++ _) // we give priority to the configuration values over the default values
+            .orElse(defaults)
+            .map(_.values.toSet)
+
+        pattern.copy(parameters = parameters)
+      })
     }
   }
 
